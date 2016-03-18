78163f46d4b69db5f77f8fe33f44ab46e44ee67d
Etienne <etienne@ThinkPax>
2016-03-17 22:33:38 -0400

    feat: Add sticky_navbar.js constructor

--- src/js/sticky_navbar.js
+++ src/js/sticky_navbar.js
@@ -4,7 +4,7 @@
    It doesn’t make a new navbar element but only acts on an already present one.sticky means that it has normal flow until the element is completly at the top. The element then becomes fixed.
 
    Code:
- */
+*/
 
 /**
  * Make a navbar "sticky". When the navbar is on the top, it becomes fixed and return normally when its static offset is reched again (srolling up). It’s the responsability of the user to implement event listeners (onscroll and onresize).
@@ -12,6 +12,15 @@
  * @param {String} sel - The navbar to use.
  * @param {String} [paddingTop=$(sel).offset().top] - Padding added to the body when the navbar quit the normal flow (is fixed).
  */
+var StickyNavbar = function(sel, paddingTop) {
+    this.navbar = $(sel);
+    this.sticked = false;
+    this.distance = this.navbar.offset().top;
+    this.paddingTop = paddingTop || this.navbar.innerHeight();
+    this.$window = $(window);
+
+    this.check();
+};
 
 /**
  * Check if the navbar should be fixed or static (normal flow).
